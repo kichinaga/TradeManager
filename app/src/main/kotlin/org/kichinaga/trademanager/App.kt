6fecb9415f2045d9b2554ea9314c53674fff16bd
kichinaga <pixo.3303@gmail.com>
2017-12-11 11:23:55 +0900

    add insert initial data on realm
    アプリ起動時にrealmに初期データを挿入

--- app/src/main/kotlin/org/kichinaga/trademanager/App.kt
+++ app/src/main/kotlin/org/kichinaga/trademanager/App.kt
@@ -2,6 +2,8 @@ package org.kichinaga.trademanager
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
+import org.kichinaga.trademanager.model.*
 
 /**
  * Created by kichinaga on 2017/12/05.
@@ -11,5 +13,21 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+
+        val config = RealmConfiguration.Builder()
+                .name("data.realm")
+                .initialData {
+                    it.let {
+                        this.let { source ->
+                            it.createAllFromJson(Company::class.java, source.resources.assets.open("companies.json"))
+                            it.createAllFromJson(Market::class.java, source.resources.assets.open("markets.json"))
+                            it.createAllFromJson(Industry::class.java, source.resources.assets.open("industries.json"))
+                        }
+                    }
+                }
+                .schemaVersion(0)
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        Realm.setDefaultConfiguration(config)
     }
 }
