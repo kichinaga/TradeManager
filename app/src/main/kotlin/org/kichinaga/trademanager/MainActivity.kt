9130f43709b0ad16aaae567e7e18e2ebcf927a70
kichinaga <pixo.3303@gmail.com>
2017-12-14 21:09:04 +0900

    add navigation (bottom) view
    Navigation View（横スライドで開くやつ）とNavigation Bottom View（下にあるやつ）を追加
    それによる仕様、Viewの変更（FrameLayout）を使うなど。

--- app/src/main/kotlin/org/kichinaga/trademanager/MainActivity.kt
+++ app/src/main/kotlin/org/kichinaga/trademanager/MainActivity.kt
@@ -1,47 +1,83 @@
 package org.kichinaga.trademanager
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import org.kichinaga.trademanager.extensions.PrefKeys
+import org.kichinaga.trademanager.extensions.setAccessToken
+import org.kichinaga.trademanager.extensions.setUserId
+import org.kichinaga.trademanager.fragment.StockListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        when(item.itemId){
-            R.id.companies -> {
-                startActivity(Intent(this, CompaniesActivity::class.java))
+        navigation_header.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.company_search -> {
+                    startActivity(Intent(this, CompaniesActivity::class.java))
+                }
+                R.id.logout -> {
+                    setAccessToken(this, PrefKeys.ERROR_TOKEN.name)
+                    setUserId(this, -1)
+                    finishAndRemoveTask()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+                R.id.settings -> {
+                    //todo 設定画面
+                }
             }
+            return@setNavigationItemSelectedListener false
         }
 
-        return super.onOptionsItemSelected(item)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.stock -> {
+                    replaceFragment(StockListFragment())
+                    true
+                }
+                R.id.fx -> {
+                    true
+                }
+                R.id.bitcoin -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        replaceFragment(StockListFragment())
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun replaceFragment(item: Fragment){
+        when(item){
+            is StockListFragment -> {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.container, item)
+                    commit()
+                }
+            }
+        }
     }
 }
