3c946d2ee6835886396bfd7e7d92545ca3f3b735
kichinaga <pixo.3303@gmail.com>
2017-12-18 09:50:06 +0900

    add StockList Activity
    StockList(登録企業管理モデル)を表示するActivityを作成

---
+++ app/src/main/kotlin/org/kichinaga/trademanager/fragment/StockListFragment.kt
@@ -0,0 +1,60 @@
+package org.kichinaga.trademanager.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView
+import io.realm.Realm
+import io.realm.Sort
+import kotlinx.android.synthetic.main.fragment_listview.*
+import org.kichinaga.trademanager.R
+import org.kichinaga.trademanager.api.service.StockListService
+import org.kichinaga.trademanager.extensions.getShapingList
+import org.kichinaga.trademanager.listview.StockListAdapter
+import org.kichinaga.trademanager.model.StockList
+
+/**
+ * Created by kichinaga on 2017/12/07.
+ */
+class StockListFragment: Fragment() {
+    private val realm: Realm = Realm.getDefaultInstance()
+    private val service = StockListService()
+    private var adapter: StockListAdapter? = null
+    private val listLimit = 20
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_listview, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        service.get(activity)
+                .subscribe { realmlist ->
+                    realm.executeTransaction { it.copyToRealmOrUpdate(realmlist) }
+                    val list = getShapingList(realmlist)
+                    adapter = StockListAdapter(activity, list)
+                    Handler(activity.mainLooper).post { lists_view.adapter = adapter }
+                }
+
+        lists_view.setOnScrollListener(object : AbsListView.OnScrollListener{
+            override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
+                val list = realm.where(StockList::class.java).findAllSorted("stock_code", Sort.ASCENDING)
+                if (totalItemCount > 0 && totalItemCount == (firstVisibleItem + visibleItemCount) && list.size > totalItemCount + listLimit){
+                    Handler(activity.mainLooper).post { adapter?.addItem(getShapingList(list, totalItemCount, totalItemCount + listLimit)) }
+                }
+            }
+
+            override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {}
+        })
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+}
--- app/src/main/kotlin/org/kichinaga/trademanager/model/StockList.kt
+++ app/src/main/kotlin/org/kichinaga/trademanager/model/StockList.kt
@@ -7,8 +7,8 @@ import io.realm.annotations.PrimaryKey
  * Created by kichinaga on 2017/12/05.
  */
 
-open class StockList(@PrimaryKey open var id: Int = 0,
+open class StockList(open var id: Int = 0,
                      open var activated: Boolean = false,
-                     open var stock_code: Int = 0,
+                     @PrimaryKey open var stock_code: Int = 0,
                      open var company: Company? = Company(),
                      open var stock_total: StockTotal? = StockTotal()): RealmObject()
